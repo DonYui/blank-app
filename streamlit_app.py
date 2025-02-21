524c82153359aba8dcea31942d88315dc3025087
DonYui <161346854+DonYui@users.noreply.github.com>
2025-02-21 16:25:51 +0000

    Solo poner seleccionar cantidad y muestra numero

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+cantidad = st.slider("Selecciona la cantidad")
+st.write(f'La cantidad seleccionada es: {cantidad}')
